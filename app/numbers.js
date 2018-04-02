9e854c33c0c567f12f6b6f9e5478ecfda8c3a096
Jordan Smith <techjems17@gmail.com>
2018-04-02 10:11:34 -0600

    added colors.html

--- app/numbers.js
+++ app/numbers.js
@@ -1,4 +1,5 @@
 const numbers = [0, 1, 5, 4, 7, 2, 8];
+const intergers = [2, 4, 6, 8, 10];
 
 sum = (numbers) => {
     let sum = 0;
@@ -6,4 +7,12 @@ sum = (numbers) => {
         sum += numbers[i];
     }
     return sum;
+}
+
+sum = (intergers) => {
+    let total = 0;
+    for (i = 0; i < intergers.length; i++) {
+        total += intergers[i];
+    }
+    return total;
 }
